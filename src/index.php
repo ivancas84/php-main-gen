3fb2ebf92779934fef351262e9739059a5794a9d
ivancas84 <ivancas84@gmail.com>
2020-09-21 00:36:34 -0300

    Incorporar generacion de funcion getEntityNames

--- src/index.php
+++ src/index.php
@@ -2,10 +2,10 @@
 
 //controlador para generar la estructura php de mapeo de base de datos
 require("../config/config.php"); 
-require("class/model/Db.php"); 
-
 
 require_once("Tablas.php");
 $gen = new Tablas();
-$gen->db = new Db();
-$gen->generate();
+
+$gen->entities();
+$gen->fields();
+$gen->functionGetEntityNames();
